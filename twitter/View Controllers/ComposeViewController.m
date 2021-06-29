aa6e2943424c1dd0c6270bcbdd6cb7a16c985605
Rigre Reinier Garciandia Larquin <rigrergl@fb.com>
2021-06-28 23:48:08 -0500

    created initial layout of compose view

---
+++ twitter/View Controllers/ComposeViewController.m
@@ -0,0 +1,38 @@
+//
+//  ComposeViewController.m
+//  twitter
+//
+//  Created by Rigre Reinier Garciandia Larquin on 6/28/21.
+//  Copyright © 2021 Emerson Malca. All rights reserved.
+//
+
+#import "ComposeViewController.h"
+
+@interface ComposeViewController ()
+
+@end
+
+@implementation ComposeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (IBAction)closeButtonClicked:(UIBarButtonItem *)sender {
+}
+
+- (IBAction)tweetButtonClicked:(UIBarButtonItem *)sender {
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
