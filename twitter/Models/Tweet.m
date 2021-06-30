a027e658cb0cea2c07186fce823364b8222b70b5
Rigre Reinier Garciandia Larquin <rigrergl@fb.com>
2021-06-30 14:44:06 -0500

    included DateTools pod and used to format date string for tweets

--- twitter/Models/Tweet.m
+++ twitter/Models/Tweet.m
@@ -8,6 +8,7 @@
 
 #import "Tweet.h"
 #import "User.h"
+#import "DateTools.h"
 
 @implementation Tweet
 
@@ -42,11 +43,9 @@
         formatter.dateFormat = @"E MMM d HH:mm:ss Z y";
         // Convert String to Date
         NSDate *date = [formatter dateFromString:createdAtOriginalString];
-        // Configure output format
-        formatter.dateStyle = NSDateFormatterShortStyle;
-        formatter.timeStyle = NSDateFormatterNoStyle;
-        // Convert Date to String
-        self.createdAtString = [formatter stringFromDate:date];
+        //Getting the short time diff string
+        NSString *timePassed = [date shortTimeAgoSinceNow];
+        self.createdAtString = timePassed;
     }
     return self;
 }
